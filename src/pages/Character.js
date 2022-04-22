e1146fce8001fc7f2746626c0d6d12044664dfbd
Thomas Bernal <thhomasgt99@gmail.com>
2022-04-21 20:06:09 -0500

    Se elimino el archivo de travis por problemas en la integracion continua, se arreglaron estilos

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -10,7 +10,7 @@ const Character = async () => {
         <img src="${character.image}" alt="${character.name}">
         <h2>${character.name}</h2>
       </article>
-      <article class="Characters-card">
+      <article class="Characters-card information">
         <h3>Episodes: <span>${character.episode.length}</span></h3>
         <h3>Status: <span>${character.status}</span></h3>
         <h3>Species: <span>${character.species}</span></h3>

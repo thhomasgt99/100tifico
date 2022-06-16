0a6ee2b2f872b1f33b1dacd5dcab80b30aea17a1
Thomas Bernal <thhomasgt99@gmail.com>
2022-06-16 14:19:45 -0500

    Cambio de algunos estilos, persiste el problema del scroll orizontal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,10 +6,20 @@ const Home = async () => {
     <div class="Characters">
       ${characters.results.map(character => `
         <article class="Character-item">
-          <a href="#/${character.id}/">
-            <img src="${character.image}" alt="${character.name}">
-            <h2>${character.name}</h2>
-          </a>
+            <div class="circle1"></div>
+            <div class="circle2"></div>
+            <div class="circle3"></div>
+
+            <div class="circle4"></div>
+            <div class="circle5"></div>
+            <div class="circle6"></div>
+            <div class="circle7"></div>
+            <a href="#/${character.id}/">
+              <div class="Character-item_degrade"></div>
+              <img src="${character.image}" alt="${character.name}">
+              <h2>${character.name}</h2>
+            </a>
+          
         </article>
       `).join('')}
     </div>
